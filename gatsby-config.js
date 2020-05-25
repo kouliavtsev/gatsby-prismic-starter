3859e993e95c23ea091554e24de13aa3ccfd0f44
Anton Kouliavtsev <seoanton@gmail.com>
2020-05-25 21:07:32 +0200

    chore: set up & source Prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-prismic`,
+      options: {
+        repositoryName: `stoked-prismic-starter`,
+        accessToken: `${process.env.API_KEY}`,
+        linkResolver: ({ node, key, value }) => post => `/${post.uid}`,
+        schemas: {
+          post: require("./src/schemas/post.json"),
+        },
+      },
+    },
+  ],
 }
