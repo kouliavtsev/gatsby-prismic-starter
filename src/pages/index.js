e527266c1b364e1139b3c49cb738dc38ded064bb
Anton Kouliavtsev <seoanton@gmail.com>
2020-06-02 20:42:10 +0200

    fix: set key on slice map

--- src/pages/index.js
+++ src/pages/index.js
@@ -49,13 +49,11 @@ const Slices = ({ slices }) => {
             <section key={slice + index}>
               <h1>{RichText.asText(slice.primary.team_section)}</h1>
 
-              {slice.fields.map(item => {
+              {slice.fields.map((item, index) => {
                 return (
-                  <div>
+                  <div key={item + index}>
                     <h2>{RichText.asText(item?.first_and_lastname)}</h2>
                     <p>{RichText.asText(item?.position)}</p>
-
-                    {/* TODO: Preprocessing External Images */}
                     <img
                       src={item?.portrait?.url}
                       alt={RichText.asText(item?.first_and_lastname)}
@@ -73,8 +71,6 @@ const Slices = ({ slices }) => {
           return (
             <section key={slice + index}>
               <h1>Quote of the day</h1>
-
-              {/* TODO: Preprocessing External Images */}
               <img
                 src={slice?.primary?.portrait_author?.url}
                 alt={RichText.asText(slice?.primary?.name_of_the_author)}
